e7ea4c803b7b2cc4460abdfef1e66a602079ca3d
AsafCX <asaf.kalimi@checkmarx.com>
2020-12-01 11:53:29 +0200

    Fix Json Dto

--- src/main/java/com/checkmarx/dto/github/WebhookGithubDto.java
+++ src/main/java/com/checkmarx/dto/github/WebhookGithubDto.java
@@ -1,6 +1,7 @@
 package com.checkmarx.dto.github;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -29,8 +30,10 @@ public @Data class WebhookGithubDto {
     public static class Config {
 
         private String url;
+        @JsonProperty("content_type")
         private String contentType;
         private String secret;
+        @JsonProperty("insecure_ssl")
         private String insecureSsl;
 
     }
